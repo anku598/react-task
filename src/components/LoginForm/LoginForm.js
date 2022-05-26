5ec004e77f205d7ba8a413b602cfadef32f8fec9
anik <anik598@gmail.com>
2022-05-26 15:44:56 +0600

    lastet and update

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './LoginForm.scss'
 function LoginForm() {
+
+  const [inputValue, setInputValue] = React.useState('');
+  const [errors, setErrors] = React.useState({});
+
+  function validateEmail(value) {
+    let errors = {};
+    if (value === '') {
+      errors.email = 'Email address is required';
+    } else if (!/\S+@\S+\.\S+/.test(value)) {
+      errors.email = 'Email address is invalid';
+    }
+    return errors;
+  };
+
+
+  function onChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function onBlur(e) {
+    setErrors(validateEmail(e.target.value));
+  }
+
   return (
     <div className='login-container'>
       <div className="form-wrapper">
@@ -9,15 +32,20 @@ function LoginForm() {
 
         <form action="#" className="form">
           <div class="form-row">
-            <input type="text" name="name" id="name" placeholder="Your Name" />
+            <input type="text" name="name" id="name" autocomplete="off" required />
+
+            <label for="name">Name</label>
           </div>
           <div class="form-row">
-            <input type="email" name="email" id="email" placeholder="Email Address" />
+            <input type="email" name="email" id="email" autocomplete="off" required onChange={onChange} onBlur={onBlur} value={inputValue} />
+            {errors.email && <div>{errors.email}</div>}
+            <label for="email">Email</label>
           </div>
           <div className="form-row">
-            <input type="password" placeholder="password" />
-            <div className="alert-info">Minimum 8 characters</div>
+            <input type="password" id='password' autocomplete="off" required />
+            <label for="password">Password</label>
           </div>
+          <div className="alert-info">Minimum 8 characters</div>
           <button className='submit-btn' disabled> Next</button>
         </form>
         <p className="submit-info">
